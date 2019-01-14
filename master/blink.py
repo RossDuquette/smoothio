3bfc6d8b1f309aa5c5549369bdc4b1d2248b3813
catoenm <mitchellcatoen@gmail.com>
2019-01-14 18:50:02 +0000

    Flask blink added

--- master/blink.py
+++ master/blink.py
@@ -3,11 +3,27 @@
 
 from gpiozero import LED
 from time import sleep
+from flask import Flask
 
-led = LED(19)
+app = Flask(__name__)
 
-while True:
-    led.on()
-    sleep(1)
-    led.off()
-    sleep(1)
+@app.route('/')
+def blink_red():
+    led = LED(16)
+
+    while True:
+        led.on()
+        sleep(1)
+        led.off()
+        sleep(1)
+
+if __name__ == '__main__':
+
+    app.run(host= '0.0.0.0')
+    led = LED(19)
+
+    while True:
+        led.on()
+        sleep(1)
+        led.off()
+        sleep(1)
