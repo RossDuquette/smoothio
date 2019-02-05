429ac9fe6b005d343e9f5f3a94266750526690aa
catoenm <mitchellcatoen@gmail.com>
2019-02-05 18:17:24 +0000

    Testing tool dispense

--- master/master_main.py
+++ master/master_main.py
@@ -14,10 +14,45 @@ reg_write_dac = 0x40
 # Initialize I2C (SMBus)
 bus = smbus.SMBus(channel)
 
+def print_lookup_table():
+    print """
+        Module Numbers
+        --------------
+        Blender:  1
+        Carousel: 2
+        Dispense: 3
+
+        Selector Numbers
+        --------------
+        *NDR = No Data Required
+        Blender:
+            0: Blender One
+            1: Blender Two
+            2: Pivot/Elevator
+        Carousel:
+            0: Rotate CW
+            1: Rotate CCW
+        Dispense:
+            0-4: Dispenses 1-5
+            5: Cup Dispense
+
+        Data Segments
+        -------------
+        Blender:
+            1: Idle
+            2: Run Blender Motor
+        Carousel: 
+            N: Number of cups to rotate
+        Dispense:
+            1: Idle
+            2: Dispense
+    """
+
 def main():
+    print_lookup_table()
     while True:
         # Receives the data from the User
-        uin = input("Enter a module. (1: Blender 2: Carousel 3: Dispense)\n")
+        uin = input("Module number (1: Blender 2: Carousel 3: Dispense): ")
         if uin == 1:
             add = mods.Blender.BLENDER_ADD
         elif uin == 2:
@@ -27,10 +62,14 @@ def main():
         else:
             print "Wrong input. Please try again"
             continue
-        head = input("Enter a header (ie 1: dispense one, 2: dispense two, ..., 6: cup dispense\n")
-        data = input("Enter data to be sent (ie 1: idle, 2: dispense fruit, 3: dispense cup\n")
+        head = input("Selector number: ")
+        data = input("Data segment: ")
         # Write out I2C command: address, reg_write_dac, msg[0], msg[1]
-        print "Sending command to node..."
+        print """
+        ###########################
+        # Sending command to node #
+        ###########################
+        """
         bus.write_i2c_block_data(add, head, [data])
 
 if __name__ == '__main__':
