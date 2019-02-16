58b2ca116734da8512e3ecfc3954c88b7d5dde78
Mitchell Catoen <mitchellcatoen@gmail.com>
2019-02-15 23:28:26 -0500

    More carousel stuff
    compiling but I can't figure out time function, sorry I'm on an airplane
    
    Need to figure out time
    
    Fixed
    
    Sensor updating

--- slaves/carousel/carousel.ino
+++ slaves/carousel/carousel.ino
@@ -4,12 +4,13 @@
 ********************************************************/
 
 #include <Wire.h>
-#include "config.h"
 #include "peripherals.h"
 
 DRV8825 stepper(MOTOR_STEPS, DIR, STEP, nEN, M0, M1, M2);
-char number[50];
-uint8_t cup_offset = 0;
+float rot_stop_time = 0;
+float next_sens_update = 0; 
+
+state_t states;
 
 void setup() {
     // initialize i2c as slave
@@ -24,14 +25,48 @@ void setup() {
     Wire.onRequest(sendData);
 }
 
-bool pin_setup() {}
+bool pin_setup() {
+    pinMode(CURR_SENSE0, INPUT);
+    pinMode(CURR_SENSE1, INPUT); 
+    pinMode(CUP_MASS0, INPUT);
+    pinMode(CUP_MASS1, INPUT);
+    pinMode(CAROUSEL_POS, INPUT);
+    pinMode(CUP_SENSE0, INPUT);
+    pinMode(CUP_SENSE1, INPUT);
+}
 
 void loop() {
-    if (state_t.num_cups != 0) {
-        carousel_rotate(state_t.num_cups);
-        delay(state_t.num_cups * CUP_WAIT_TIME);
-        state_t.num_cups = 0;
+    if (states.num_cups != 0) {
+        // Not sure if time() is the right call
+        rot_stop_time = millis() + CUP_WAIT_TIME*states.num_cups;
+        carousel_rotate(states.c_state, states.num_cups);
     }
+    switch(states.c_state) {
+        case C_IDLE:
+            break;
+        case MOVING_CW:
+            if (millis() > rot_stop_time)
+                states.c_state = C_IDLE;
+            break;
+        case MOVING_CCW:
+            if (millis() > rot_stop_time)
+                states.c_state = C_IDLE; 
+            break;
+    }
+    if (millis() > next_sens_update) {
+        read_sensors();
+        next_sens_update = next_sens_update + SENS_UPDATE_TIME;
+    }
+}
+
+void read_sensors() {
+    states.cup_sense0 = analogRead(CUP_SENSE0);
+    states.cup_sense1 = analogRead(CUP_SENSE1);
+    states.curr_sense0 = analogRead(CURR_SENSE0);
+    states.curr_sense1 = analogRead(CURR_SENSE1);
+    states.cup_mass0 = analogRead(CUP_MASS0);
+    states.cup_mass1 = analogRead(CUP_MASS1);
+    states.carousel_pos = analogRead(CAROUSEL_POS);
 }
 
 // callback for received data
@@ -40,8 +75,8 @@ void receiveData(int byteCount) {
         COMM_SELECTOR selector = (COMM_SELECTOR)Wire.read();
         if (Wire.available()) {
             uint8_t data = Wire.read();
-            state_t.direction = selector;
-            state_t.num_cups = data
+            states.c_state = selector;
+            states.num_cups = data;
         }
     }
 }
@@ -49,7 +84,7 @@ void receiveData(int byteCount) {
 // callback for sending data
 void sendData() {
     // send back state to master
-    Wire.write((const char*)&states, sizeof(state_t));
+    Wire.write((const char*)&states, sizeof(states));
 }
 
 bool stepper_init(uint8_t microsteps) {
@@ -68,7 +103,11 @@ bool stepper_init(uint8_t microsteps) {
 
 bool carousel_home() { return true; }
 
-bool carousel_rotate(uint8_t n) {
-    stepper.rotate(72 * n);  // 360 degrees / 5
-    return true;
+bool carousel_rotate(uint8_t dir, uint8_t n) {
+    if (dir == CW) {
+        stepper.rotate(72 * n);  // 360 degrees / 5
+        return true;
+    }
+   stepper.rotate(-72 * n);  // 360 degrees / 5
+   return true; 
 }
--- slaves/carousel/config.h
+++ slaves/carousel/config.h
@@ -9,22 +9,34 @@ typedef enum COMM_SELECTOR {  // Decode message from master
     RESET = 255
 } COMM_SELECTOR;
 
+typedef enum CAROUSEL {
+    C_IDLE = 0,
+    MOVING_CW,
+    MOVING_CCW
+} CAROUSEL;
+
 // State variables, for communication
 typedef struct state_t {
     // Outputs
-    uint8_t direction;
+    uint8_t c_state;
     uint8_t num_cups;
 
     // Inputs
+    uint8_t cup_sense0;
     uint8_t cup_sense1;
-    uint8_t cup_sense2;
+    uint8_t curr_sense0;
+    uint8_t curr_sense1;
+    uint8_t cup_mass0;
+    uint8_t cup_mass1;
+    uint8_t carousel_pos;
 } state_t;
 
 // Slave Address for the Communication
 #define SLAVE_ADDRESS 0x05
 
 // Misc
-#define CUP_WAIT_TIME 1000;
+#define CUP_WAIT_TIME 1000
+#define SENS_UPDATE_TIME 100
 
 // Pin Layout
 #define DIR 12
@@ -44,11 +56,14 @@ typedef struct state_t {
 #define ADD_A13 A13
 #define ADD_A12 A12
 
-#define CURR_SENS1 A7
-#define CURR_SENS0 A6
+#define CURR_SENSE1 A7
+#define CURR_SENSE0 A6
 #define CUP_MASS1 A5
 #define CUP_MASS0 A4
 
 #define CAROUSEL_POS A2
 #define CUP_SENSE1 A1
 #define CUP_SENSE0 A0
+
+// Function defs
+float get_time();
