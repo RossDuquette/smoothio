cdcaaf4fec380862b790cb005a00ce8428cd88cc
Mitchell Catoen <mitchellcatoen@gmail.com>
2019-03-06 13:22:10 -0500

    Scheduler

--- master/module/scheduler.py
+++ master/module/scheduler.py
@@ -56,20 +56,31 @@ class Scheduler:
                 # Wait for cup to be taken 
                 self.cup_states[4] = False
 
-        all_stations_go = True
         while not all_stations_go:
-            if time.time() >= self.frozen_time:
+            # Check cup dispense
+            if not self.cup_states[0] and cup_dispense_done():
+                self.cup_states[0] = True
+            # Check frozen dispense
+            if not self.cup_states[1] and time.time() >= self.frozen_time:
                 self.dispense.send_command(1, 0)
                 self.dispense.send_command(2, 0)
                 self.dispense.send_command(3, 0) 
                 self.cup_states[1] = True
-            if time.time() >= self.liquid_time:
+            # Check liquid dispense
+            if not self.cup_states[2] and time.time() >= self.liquid_time:
                 self.dispense.send_command(4, 0)
                 self.dispense.send_command(5, 0)
                 self.dispense.send_command(6, 0) 
                 self.cup_states[2] = True
+            # Check blender
+            if not self.cup_states[3] and blender_done():
+                self.cup_states[3] = True
+            # Check if cup has been taken
+            if not self.cup_states[4] and cup_serve_done():
+                self.cup_states[4] = True
 
             # Check if all states are idle
+            all_stations_go = True
             for cs in self.cup_states:
                 if cs == False:
                     all_stations_go = False
@@ -82,6 +93,21 @@ class Scheduler:
             else:
                 self.cup_posns[i] += 1
 
-    def blender_state(self):
-        return 
+    def blender_done(self):
+        self.blender.read_data()
+        if self.blender.routine == 0:
+            return True
+        return False
 
+    def cup_dispense_done(self):
+        self.dispense.read_data()
+        if self.dispense.cup == 0:
+            return True
+        return False
+
+    def cup_serve_done(self):
+        self.carousel.read_data()
+        # @ROSS pls check this this is the correct cup-sense
+        if self.carousel.cup_sense0 == 0:
+            return True
+        return False
