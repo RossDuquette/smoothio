1c8bc330fc66c2bed8cc8848e77c1e9f5443f777
David Ferris <david.j.ferris.21@gmail.com>
2019-03-12 00:27:40 -0400

    Ordering javascript and form parsing complete.

--- ordering_system/order.js
+++ ordering_system/order.js
@@ -1,15 +1,38 @@
 const url = "smoothio-endpoint.com/order";
-const data = {
-    id: 123,
-    order: 456
-};
 
-function sendOrderRequest() {
-    alert('Order recieved.');
+function stripFormData() {
+    var solidDispenseOptions = [false, false, false];
+    var liquidDispenseOptions = [false, false, false];
 
-    $.post(url, data, function (data, status) {
-        console.log('${status}');
-    });
+    solidDispenseOptions[0] = document.getElementById("solid-dispense1").checked;
+    solidDispenseOptions[1] = document.getElementById("solid-dispense2").checked;
+    solidDispenseOptions[2] = document.getElementById("solid-dispense3").checked;
+
+    liquidDispenseOptions[0] = document.getElementById("liquid-dispense1").checked;
+    liquidDispenseOptions[1] = document.getElementById("liquid-dispense2").checked;
+    liquidDispenseOptions[2] = document.getElementById("liquid-dispense3").checked;
+
+    return {
+        solid: solidDispenseOptions,
+        liquid: liquidDispenseOptions
+    };
 }
 
-$('.your-checkbox').prop('indeterminate', true)
+function validateFormData(data) {
+    // TODO: Ensure that a request contains valid parameters (ex. minimum selection criteria)
+    return true;
+}
+
+function sendOrderRequest() {
+    var formData = stripFormData();
+    if (!validateFormData(formData)) {
+        alert("Invalid Order!")
+        return;
+    }
+
+    alert("Attempting Request");
+    $.post(url, formData, function (data, status) {
+        console.log('${status}');
+    });
+    alert("Rquest Successful");
+}
