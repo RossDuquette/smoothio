c5859131df322e0acbc28b3677bae9ff8b8f4f95
catoenm <mitchellcatoen@gmail.com>
2019-03-15 04:47:26 +0000

    Secrets consumed

--- master/main.py
+++ master/main.py
@@ -3,13 +3,15 @@
 ######################
 
 import module.scheduler as scdr
-from flask import Flask  
+from flask import Flask, request, jsonify
 import thread
 import time
+import json
 
 app = Flask(__name__)
 scheduler = scdr.Scheduler()
 smoothie_queue = []
+secrets = set()
 
 def intro():
     print("""
@@ -18,51 +20,78 @@ def intro():
     ---------------------
     """)
 
-@app.route("/enqueue")
+@app.route("/enqueue", methods=['POST'])
 def enqueue():
     print("Enqueuing Smoothie")
     smoothie_queue.append(0)
-    return "Request Satisfied"
+    content = request.json
+    content['posn'] = 0
+    if content['secret'] in secrets:
+        secrets.remove(content['secret'])
+    else:
+        return "Secret invalid or already used"
+    return "Request satisfied"
 
 def flask_server():
     app.run(host= '0.0.0.0')
 
+def read_secrets():
+    f = open("secrets.txt", "r")
+    lines = f.readlines()
+    for line in lines:
+        secrets.add(line.rstrip())
+    f.close()
+
 def main():
-    intro() 
-    print "Homing Everything"
-    scheduler.home_everything()
-    resp = input("Send any command when done homing: ")
+    try:
+        intro() 
+        read_secrets()
+        while True:
+            time.sleep(1)
+            print secrets
 
-    carousel_spinning = False
-    # while scheduler.empty():
-    #     pass
-    smoothie_queue.append(0)
-    # smoothie_queue.append(0)
-    # smoothie_queue.append(0)
 
-    # Run scheduler until the smoothie has been made
-    while True:
-        print scheduler.cup_posns
-        if scheduler.empty() and len(smoothie_queue) > 0:
-            smoothie_queue.pop()
-            scheduler.cup_posns.append(0)
-            scheduler.add_cup(0) 
-        if carousel_spinning:
-            if scheduler.check_carousel_idle():
-                carousel_spinning = False
-                scheduler.shift_cups()
-                if len(smoothie_queue) > 0:
-                    smoothie_queue.pop()
-                    scheduler.cup_posns.append(0)
-                    scheduler.add_cup(0)
-        else:
-            scheduler.update()
-            if scheduler.check_all_stations_go():
-                if not scheduler.empty():
-                    scheduler.start_carousel_spin()
-                    carousel_spinning = True
-        
-        time.sleep(0.1)
+        print "Homing Everything"
+        scheduler.home_everything()
+        resp = input("Send any command when done homing: ")
+
+        carousel_spinning = False
+        # while scheduler.empty():
+        #     pass
+        smoothie_queue.append(0)
+
+        # Run scheduler until the smoothie has been made
+        while True:
+            print scheduler.cup_posns
+            if scheduler.empty() and len(smoothie_queue) > 0:
+                smoothie_queue.pop()
+                scheduler.cup_posns.append(0)
+                scheduler.add_cup(0) 
+            if carousel_spinning:
+                if scheduler.check_carousel_idle():
+                    carousel_spinning = False
+                    scheduler.shift_cups()
+                    if len(smoothie_queue) > 0:
+                        smoothie_queue.pop()
+                        scheduler.cup_posns.append(0)
+                        scheduler.add_cup(0)
+            else:
+                scheduler.update()
+                if scheduler.check_all_stations_go():
+                    if not scheduler.empty():
+                        scheduler.start_carousel_spin()
+                        carousel_spinning = True
+            
+            time.sleep(0.1)
+    except:
+        print("Resetting Modules")
+        # schedule.reset_everything()
+        print("Printing Current ")
+        f = open("secrets.txt","w") 
+        for secret in secrets:
+            f.write(secret + "\n")
+        f.close()
+        # Write the text file out
 
 if __name__ == "__main__":
     thread.start_new_thread(flask_server,())
